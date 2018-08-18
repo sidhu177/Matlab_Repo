96a174b7ed54d7a7394b23713671c3036f89e9ec
Sidhu177 <sidharth177@gmail.com>
2018-08-17 23:48:05 -0400

    ME522 Advanced Finite code

---
+++ prob1b_ME522.m
@@ -0,0 +1,20 @@
+close all
+clc
+
+j=0:0.01:1
+f=j+2*j.^3;
+plot(j,f)
+ylabel force;
+xlabel x;
+grid on
+
+x=0;
+Gp=1+6*x^2;
+for i=1:5
+    G=x+2*x^3-1;
+
+    dx=-G/Gp;
+    x=x+dx;
+    fprintf('G=%4.2f,Gp=%5.3f,dx=%5.3f,x=%4.4f\n',G,Gp,dx,x);
+end
+    
